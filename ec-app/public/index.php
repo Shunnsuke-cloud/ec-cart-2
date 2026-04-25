70a3cee430e75c9e4000b94f5a0921706a9d5a0e
Shunnsuke-cloud <u.shunke@gmail.com>
2026-04-25 10:13:28 +0900

    共通レイアウト化

--- ec-app/public/index.php
+++ ec-app/public/index.php
@@ -1,1 +1,12 @@
 <?php
+$pageTitle = 'トップ';
+$activePage = 'home';
+require_once __DIR__ . '/../views/layout/header.php';
+?>
+
+<section>
+	<h2>ようこそEC Cartへ</h2>
+	<p>このページは共通レイアウトを使って表示されています。</p>
+</section>
+
+<?php require_once __DIR__ . '/../views/layout/footer.php'; ?>
